5070c956f138792161e8f6103e48889ae399dad6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-19 09:07:51 +0000

    Deploy compulim/react-say to github.com/compulim/react-say.git:gh-pages

---
+++ precache-manifest.d3ec1104996a3f84a3aacd6287f9480d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161ecd0bbc4086fc67636c8f22dd4283",
+    "url": "/react-say/index.html"
+  },
+  {
+    "revision": "342111dddf7ef690ce92",
+    "url": "/react-say/static/js/2.c6b263c0.chunk.js"
+  },
+  {
+    "revision": "19553cd882c53bef3aa9",
+    "url": "/react-say/static/js/main.dc058352.chunk.js"
+  },
+  {
+    "revision": "8d29972b22aa9c45d935",
+    "url": "/react-say/static/js/runtime~main.9c942dd3.js"
+  }
+]);
